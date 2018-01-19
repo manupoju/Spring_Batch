8f3d8f191048e04eb8c78a3613f2f4d84f03e3de
DESKTOP-Q3J1CKV\NISUM <manupoju@nisum.com>
2018-01-19 10:43:26 +0530

    Spring Batch POC for improvement area(CSV to MySql)

---
+++ src/main/java/com/javasampleapproach/batchcsvpostgresql/controller/WebController.java
@@ -0,0 +1,29 @@
+package com.javasampleapproach.batchcsvpostgresql.controller;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	@Autowired
+	JobLauncher jobLauncher;
+
+	@Autowired
+	Job job;
+
+	@RequestMapping("/runjob")
+	public String handle() throws Exception {
+		try {
+			JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+					.toJobParameters();
+			jobLauncher.run(job, jobParameters);
+		} catch (Exception e) {
+		}
+		return "Done! Check Console Window for more details";
+	}
+}
